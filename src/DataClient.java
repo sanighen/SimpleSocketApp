bbaa5d0343e16fa14efd30479e7be2e8452fa2e8
sanighen <684257913>
2020-12-13 16:21:02 +0200

    simple socket app

---
+++ src/DataClient.java
@@ -0,0 +1,57 @@
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataClient {
+
+    private List<Integer> data;
+
+    public DataClient(List<Integer> data) {
+        this.data = data;
+    }
+
+    public void getData() {
+
+        try (Socket socket = new Socket("localhost", DataServer.PORT)) {
+            System.out.println("CLIENT >>> starting");
+            DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
+            String msg = "";
+
+            for (int i = 0; i < data.size(); i++) {
+                msg += data.get(i);
+                if (i != data.size() - 1)
+                    msg += ", ";
+            }
+
+            outStream.writeUTF(msg);
+            outStream.flush();
+
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            System.out.println("CLIENT >>> server sent: " + inputStream.readUTF());
+
+            socket.close();
+            outStream.close();
+            inputStream.close();
+            System.out.println("CLIENT >>> ending");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>() {{
+            add(10);
+            add(20);
+            add(30);
+        }};
+        DataClient dc = new DataClient(list);
+        dc.getData();
+    }
+
+}
---
+++ src/DataServer.java
@@ -0,0 +1,46 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataServer {
+
+    public static final int PORT = 8888;
+
+    public static void main(String[] args) throws IOException {
+        DataServer.start();
+    }
+
+    public static void start() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("SERVER >>> starting");
+        DataInputStream inputStream = new DataInputStream(clientSocket.getInputStream());
+        String number = inputStream.readUTF();
+        System.out.println("SERVER >>> client sent: " + number);
+        String[] numbers = number.split(", ");
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < numbers.length; i++) {
+            list.add(Integer.parseInt(numbers[i]));
+        }
+        DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
+        outputStream.writeUTF(findAverage(list));
+
+        serverSocket.close();
+        clientSocket.close();
+        inputStream.close();
+        System.out.println("SERVER >>> ending");
+    }
+
+    public static String findAverage(List<Integer> list) {
+        int avg = 0;
+        for (Integer i : list)
+            avg += i;
+        avg /= list.size();
+        return String.valueOf(avg);
+    }
+
+}
